c846e55b2e6b40680c86da384b8681d43810f774
Yehuda Lehrfield <yll2113@columbia.edu>
2022-12-28 09:37:21 -0500

    feat: functional tests

--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,10 @@ module.exports = (app) => {
     const { text, locale } = req.body;
     let errorMessage;
     let result;
-    if (!text || !locale) errorMessage = 'Required field(s) missing';
+    if (text === undefined || locale === undefined)
+      errorMessage = 'Required field(s) missing';
     // not sure about this one. "If text is empty..." ? How is this different than above?
-    // else if (text.split(' ' < 1)) errorMessage = 'No text to translate';
+    else if (!text) errorMessage = 'No text to translate';
     else if (
       locale !== 'american-to-british' &&
       locale !== 'british-to-american'
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,92 @@
+/* eslint-disable no-undef */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const assert = chai.assert;
-const server = require('../server.js');
+
+const { assert } = chai;
+const server = require('../server');
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+// const Translator = require('../components/translator');
 
 suite('Functional Tests', () => {
-
+  test('Translate with text and locale fields', (done) => {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({
+        text: 'Mangoes are my favorite fruit.',
+        locale: 'american-to-british',
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'translation');
+        assert.equal(
+          res.body.translation,
+          'Mangoes are my <span class="highlight">favourite</span> fruit.'
+        );
+      });
+    done();
+  });
+  test('Translate with missing text and locale fields', (done) => {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'Required field(s) missing');
+      });
+    done();
+  });
+  test('Translate with missing text field', (done) => {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({ locale: 'american-to-british' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'Required field(s) missing');
+      });
+    done();
+  });
+  test('Translate with missing locale field', (done) => {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({ text: 'Translator' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'Required field(s) missing');
+      });
+    done();
+  });
+  test('Translate with empty text field', (done) => {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({ text: '', locale: 'american-to-british' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'No text to translate');
+      });
+    done();
+  });
+  test('Translate with empty text field', (done) => {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({ text: 'Hey there!', locale: 'american-to-british' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'translation');
+        assert.property(res.body, 'text');
+        assert.equal(res.body.translation, 'Everything looks good to me!');
+      });
+    done();
+  });
 });
